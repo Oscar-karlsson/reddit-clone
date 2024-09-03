70467fda1fa39166a72d5494f629443f3faed2f3
Oscar <oscar_karlsson@live.se>
2024-09-03 18:13:34 +0200

    Initial commit

---
+++ types/types.d.ts
@@ -0,0 +1,30 @@
+type ThreadCategory = "THREAD" | "QNA";
+
+type User = {
+	userName: string;
+	password: string
+}
+
+type Thread = {
+  id: number;
+	title: string;
+	category: ThreadCategory;
+	creationDate: string;
+	description: string;
+	creator: User;
+	commentCount: number;
+}
+
+type QNAThread =  Thread & { //Type extension
+	category: "QNA";
+	isAnswered: boolean;
+	commentAnswerId?: number;
+}
+
+type ThreadComment = {
+	id: number;
+	thread: number;
+	content: string;
+	creator: User
+	creationDate: string;
+}
